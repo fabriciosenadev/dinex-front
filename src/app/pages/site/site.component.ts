f46673af42edbff5d6090667c5f8c4819c2a12a5
Fabricio Sena <fabricior.sena@outlook.com>
2022-02-13 20:45:49 -0300

    implement structure to register new user by site

--- src/app/pages/site/site.component.ts
+++ src/app/pages/site/site.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/services/user/user.service';
 
 @Component({
   selector: 'app-site',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiteComponent implements OnInit {
 
-  constructor() { }
+  email: string = '';
+
+  set userEmail(email: string) {
+    this.userService.userRegister.email = email;
+  }
+
+  constructor(
+    private router: Router,
+    private userService: UserService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  startUserRegister(): void {
+    this.userEmail = this.email;
+    this.router.navigate(['/register']);
   }
 
 }
