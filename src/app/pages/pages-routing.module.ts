26ce910a41308e1a26a0c7ae3fccadcdd8a35a40
Fabricio Sena <fabricior.sena@outlook.com>
2022-02-13 20:10:47 -0300

    add site to initial route

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SiteComponent } from './site/site.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SiteComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
