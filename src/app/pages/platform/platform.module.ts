2db9771219dee047865005a6aed2b613fdb19f3e
Fabricio Sena <fabricior.sena@outlook.com>
2022-02-13 21:17:50 -0300

    add internal platform module

---
+++ src/app/pages/platform/platform.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlatformRoutingModule } from './platform-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MainComponent } from './main/main.component';
+import { ComponentsModule } from 'src/app/components/components.module';
+
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [
+    CommonModule,
+    PlatformRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ComponentsModule
+  ]
+})
+export class PlatformModule { }
