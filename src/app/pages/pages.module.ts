4bc5f12c060f3e3c3882afab684a49c79c62dc43
Fabricio Sena <fabricior.sena@outlook.com>
2022-02-13 21:17:11 -0300

    add new components and module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HttpClientModule } from '@angular/common/http';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ComponentsModule } from '../components/components.module';
 import { SiteComponent } from './site/site.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { PlatformModule } from './platform/platform.module';
 
 
 @NgModule({
   declarations: [
-    SiteComponent
+    SiteComponent,
+    RegisterComponent
   ],
   exports: [
     
@@ -18,10 +22,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     PagesRoutingModule,
     
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
 
-    ComponentsModule    
+    ComponentsModule,
+    PlatformModule
   ]
 })
 export class PagesModule { }
