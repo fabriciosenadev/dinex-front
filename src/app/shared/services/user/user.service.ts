f3e520df8c6d0f763922b34c19d3c18265184b0f
Fabricio Sena <fabricior.sena@outlook.com>
2022-02-26 11:57:54 -0300

    add user creation using api

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
 import { UserRegister } from '../../models/user/user-register.model';
+import { User } from '../../models/user/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   
+  apiUrl = environment.apiUrl;
+
   private _userRegister = new BehaviorSubject<UserRegister>({
     fullName: '',
     email: '',
@@ -23,4 +29,13 @@ export class UserService {
   };
 
   constructor(private http: HttpClient) { }
+
+  create(userRegister: UserRegister): Observable<User> {
+    let route = `${this.apiUrl}/users`;
+    console.log(userRegister);
+    
+    return this.http.post<UserRegister>(route, userRegister);
+  }
+
+  // requestActivationCode(email: string): Observable<any> {}
 }
