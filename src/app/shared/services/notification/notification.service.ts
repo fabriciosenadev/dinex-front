42ca9d122178f3e2dae09d3ed0891314b4fd9419
Fabricio Sena <fabricior.sena@outlook.com>
2022-03-06 23:06:10 -0300

    add notification duration over env param

--- src/app/shared/services/notification/notification.service.ts
+++ src/app/shared/services/notification/notification.service.ts
@@ -1,27 +1,28 @@
 import { Injectable } from '@angular/core';
 import { NgToastService } from 'ng-angular-popup';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
-  duration5Seconds: number = 5000;
+  durationInSeconds: number = environment.durationShowNotification;
 
   constructor(private toast: NgToastService) { }
 
   success(title: string, message: string) {
-    this.toast.success({detail: title, summary: message, duration: this.duration5Seconds});
+    this.toast.success({detail: title, summary: message, duration: this.durationInSeconds});
   }
 
   error(title: string, message: string) {
-    this.toast.error({detail: title, summary: message, duration: this.duration5Seconds});
+    this.toast.error({detail: title, summary: message, duration: this.durationInSeconds});
   }
 
   warning(title: string, message: string) {
-    this.toast.warning({detail: title, summary: message, duration: this.duration5Seconds});
+    this.toast.warning({detail: title, summary: message, duration: this.durationInSeconds});
   }
 
   info(title: string, message: string) {
-    this.toast.info({detail: title, summary: message, duration: this.duration5Seconds});
+    this.toast.info({detail: title, summary: message, duration: this.durationInSeconds});
   }
 }
