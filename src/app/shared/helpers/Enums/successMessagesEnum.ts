caeb2785c0cae33c3fb38133c0e5d150d34c53c1
Fabricio Sena <fabricior.sena@outlook.com>
2022-03-06 23:07:03 -0300

    add success msg notifications

---
+++ src/app/shared/helpers/Enums/successMessagesEnum.ts
@@ -0,0 +1,11 @@
+export enum successMessagesEnum {
+    register = 'Cadastro realizado com sucesso!',
+    activate = 'Conta ativada com sucesso',
+    login = 'Login realizado com sucesso',
+    // update = 'Dados atualizados com sucesso',
+    // delete = 'Conta deletada com sucesso',
+    // password = 'Senha alterada com sucesso',
+    // forgot = 'Email enviado com sucesso',
+    // resetPassword = 'Senha alterada com sucesso',
+    // changePassword = 'Senha alterada com sucesso',
+}
