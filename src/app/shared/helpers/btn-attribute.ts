ecda90a0e224b5f03e7a3b9a9b4053de385436ce
Fabricio Sena <fabricior.sena@outlook.com>
2022-03-18 23:48:39 -0300

    fix param name

--- src/app/shared/helpers/btn-attribute.ts
+++ src/app/shared/helpers/btn-attribute.ts
@@ -1,6 +1,6 @@
 export default class BtnAttribute {
-    public static disabled(elementName: string, attributeValue: string): void {
-        let button = document.getElementById(elementName);
+    public static disabled(elementId: string, attributeValue: string): void {
+        let button = document.getElementById(elementId);
         button?.setAttribute('disabled', attributeValue);
     }
 }
