04d0c53bfd61468dcaca04f96171423f03279c7e
Fabricio Sena <fabricior.sena@outlook.com>
2022-05-14 00:30:15 -0300

    feature: add structure to return new launch

--- src/app/components/forms/launch-form/launch-form.component.ts
+++ src/app/components/forms/launch-form/launch-form.component.ts
@@ -1,6 +1,12 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faInfo } from '@fortawesome/free-solid-svg-icons';
+import { LaunchPayMethod } from 'src/app/shared/helpers/Enums/launchPayMethodEnum';
+import { launchStatus } from 'src/app/shared/helpers/Enums/launchStatusEnum';
+import { Category } from 'src/app/shared/models/category/category.model';
+import { LaunchAndPayMethodRegister } from 'src/app/shared/models/launch/register/launch-and-pay-method-register.model';
+import { launchRegister } from 'src/app/shared/models/launch/register/launch-register.model';
+import { PayMethodRegister } from 'src/app/shared/models/launch/register/pay-method-register.model';
 
 @Component({
   selector: 'app-launch-form',
@@ -9,17 +15,29 @@ import { faInfo } from '@fortawesome/free-solid-svg-icons';
 })
 export class LaunchFormComponent implements OnInit {
 
-  @Output() newLaunch: EventEmitter<any> = new EventEmitter();
+  @Input() categories: Category[] = [];
+
+  @Output() newLaunch: EventEmitter<LaunchAndPayMethodRegister> = new EventEmitter();
   @Output() openCategoryPage: EventEmitter<boolean> = new EventEmitter(false);
 
   // Icons
   faInfo = faInfo;
-  
+
   launchForm: FormGroup = new FormGroup({});
-  
+
+  selectedCategories: Category[] = [];
+
+  launchPayMethod = LaunchPayMethod;
+  showPayMethods = false;
+
+  get formData() {
+    return this.launchForm.controls;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    this.handleLaunchForm();
   }
 
   handleLaunchForm(): void {
@@ -36,7 +54,7 @@ export class LaunchFormComponent implements OnInit {
           Validators.required,
         ],
       ),
-      idCategory: new FormControl(
+      categoryId: new FormControl(
         '',
         [
           Validators.required,
@@ -53,36 +71,78 @@ export class LaunchFormComponent implements OnInit {
           Validators.min(0.01),
         ],
       ),
+      payMethod: new FormControl(
+        '',
+        []
+      ),
       isConfirmed: new FormControl(
         '',
         [],
       ),
       isScheduled: new FormControl(
         '',
-        [], 
+        [],
       ),
     });
-  }
-  
-  getBodyConfirmedHelp(): string {
-    let bodyConfirmedHelp = '<h5>Como funciona ?</h5>';
-    bodyConfirmedHelp += 'Esta funcionalidade informa se o lançamento foi recebido/pago de acordo com o que foi selecionado no tipo de lançamento.';
-    bodyConfirmedHelp += '<br><br><hr><br>';
-    bodyConfirmedHelp += '<h5>Como eu sei se ativei?</h5>';
-    bodyConfirmedHelp += 'Quando o opção estiver selecionada, o botao ficará preenchido com verde.';
-    return bodyConfirmedHelp;
-  }
-
-  getBodySchedulingHelp(): string {
-    let bodyConfirmedHelp = '<h5>Como funciona ?</h5';
-    bodyConfirmedHelp += 'Esta funcionalidade informa se o lançamento deve se repetir no proximo mês.';
-    bodyConfirmedHelp += '<br><br><hr><br>';
-    bodyConfirmedHelp += '<h5>Como eu sei se ativei?</h5>';
-    bodyConfirmedHelp += 'Quando o opção estiver selecionada, o botao ficará preenchido com verde.';
-    return bodyConfirmedHelp;
   }
 
   goToCategoryPage(): void {
     this.openCategoryPage.emit(true);
+  }
+
+  onSubmit() {
+    let newLaunchRegister: LaunchAndPayMethodRegister = {
+      launch: this.getLaunchRegister(),
+      payMethodFromLaunch: this.launchForm.value.payMethod ? this.getPayMethod() : null,
+    };
+
+    this.newLaunch.emit(newLaunchRegister);
+  }
+
+  roleForm(): void {
+    this.fillCategories();
+
+    this.showPayMethods = this.launchForm.value.launchType === 'in' ? false : true;
+  }
+
+  fillCategories(): void {
+    this.selectedCategories = this.categories
+      .filter(
+        category =>
+          category.applicable.toLowerCase() === this.launchForm.value.launchType
+      );
+  }
+
+  getLaunchRegister(): launchRegister {
+    let newLaunchRegister: launchRegister = {
+      date: this.launchForm.value.date,
+      categoryId: this.launchForm.value.categoryId,
+      description: this.launchForm.value.description,
+      amount: this.launchForm.value.amount,
+      status: this.getStatus(),
+    };
+
+    return newLaunchRegister;
+  }
+
+  getPayMethod(): PayMethodRegister {
+    let newPayMethodRegister: PayMethodRegister = {
+      payMethod: this.launchForm.value.payMethod,
+    };
+
+    return newPayMethodRegister;
+  }
+
+  getStatus(): launchStatus {
+
+    if (this.launchForm.value.isConfirmed && this.launchForm.value.launchType === 'in') {
+      return launchStatus.received;
+    }
+
+    if (this.launchForm.value.isConfirmed && this.launchForm.value.launchType === 'out') {
+      return launchStatus.paid;
+    }
+
+    return launchStatus.pending;
   }
 }
