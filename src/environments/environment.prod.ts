c2996732d5dce7190f1470d7ea5399a9ed62e853
Fabricio Sena <fabricior.sena@outlook.com>
2022-06-11 18:44:24 -0300

    experiment about env server var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api.dinheiroexato.com.br/v1",
+  // apiUrl: "https://api.dinheiroexato.com.br/v1",
+  apiUrl: apiUrl,
   durationShowNotification: 5000,
 };
