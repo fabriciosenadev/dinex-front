9b2a73da324fb33f4ca2db07668d354b0049eed5
Fabricio Sena <fabricior.sena@outlook.com>
2022-07-16 10:09:38 -0300

    replace toast with settings to the new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 // lib modules
-import { NgToastModule } from 'ng-angular-popup';
+
 
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 registerLocaleData(localePt, 'pt');
 
@@ -20,9 +22,13 @@ registerLocaleData(localePt, 'pt');
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgToastModule,
     ComponentsModule,
-    PagesModule
+    PagesModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      maxOpened: 4,
+      autoDismiss: true
+    })
   ],
   providers: [
     {
