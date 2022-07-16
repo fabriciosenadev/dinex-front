735fd408bc9dda629da61e6c6515eb3071acc013
Fabricio Sena <fabricior.sena@outlook.com>
2022-07-16 17:11:30 -0300

    add method to card calls detailed month selected

--- src/app/components/month/month-card/month-card.component.ts
+++ src/app/components/month/month-card/month-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { LaunchResumeByYearAndMonth } from 'src/app/shared/interfaces/launch/launch-resume-by-year-and-month.interface';
 
 @Component({
@@ -16,19 +16,35 @@ export class MonthCardComponent implements OnInit {
     totalAvailable: 0
   };
 
-constructor() { }
+  @Output() selectedMonthDetailPage: EventEmitter<string> = new EventEmitter();
 
-ngOnInit(): void {
-}
+  constructor() { }
 
-showResume() {
-}
+  ngOnInit(): void {
+  }
 
-getMonthNameUsingStartDate(startDate: Date) : string {
-  let date = new Date(this.monthResume.startDate);
-  let monthName = date.toLocaleString('pt-br', { month: 'long' });
-  return monthName.charAt(0).toUpperCase() + monthName.slice(1);
-}
+  showDetailedMonth(monthResume: LaunchResumeByYearAndMonth) {
+    let date = new Date(monthResume.startDate);
+    let month = date.toLocaleString('pt-br', { month: '2-digit' });
+    let year = date.toLocaleString('pt-br', { year: 'numeric' });
 
+    this.selectedMonthDetailPage.emit(`${year}/${month}`);
+  }
 
+  getMonthNameUsingStartDate(): string {
+    let date = new Date(this.monthResume.startDate);
+    let monthName = date.toLocaleString('pt-br', { month: 'long' });
+    return monthName.charAt(0).toUpperCase() + monthName.slice(1);
+  }
+
+  hasLaunch(monthResume: LaunchResumeByYearAndMonth): boolean {
+    let hasLaunch = true;
+    let hasPaing = monthResume.paid > 0;
+    let hasReceiving = monthResume.received > 0;
+
+    if (hasPaing || hasReceiving || monthResume.hasPending)
+      hasLaunch = false;
+
+    return hasLaunch;
+  }
 }
