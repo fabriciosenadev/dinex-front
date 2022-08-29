864db2850e0c9dea3054497026a9a9d07594a872
Fabricio Sena <fabricior.sena@outlook.com>
2022-08-28 22:18:18 -0300

    refactore default create base method

--- src/app/shared/services/base/base.service.ts
+++ src/app/shared/services/base/base.service.ts
@@ -3,12 +3,13 @@ import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { SessionService } from '../session/session.service';
+import { User } from '../../interfaces/user/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService<TModel> {
-  
+
   apiUrl = environment.apiUrl;
   route: string = '';
 
@@ -16,26 +17,34 @@ export class BaseService<TModel> {
     protected http: HttpClient,
     protected session: SessionService,
     @Inject('route') protected receivedRoute: string,
-  ) { 
+  ) {
     this.route = receivedRoute;
   }
 
   create(creationData: TModel): Observable<TModel> {
     let route = `${this.apiUrl}/${this.route}`;
-    
-    return this.http.post<TModel>(route, creationData);
+
+    return this.http.post<TModel>(
+      route,
+      creationData,
+      {
+        headers: {
+          Authorization: this.session.getToken()
+        }
+      }
+    );
   }
 
   get(id: number): Observable<TModel> {
     let route = `${this.apiUrl}/${this.route}/${id}`;
     return this.http.get<TModel>(
-        route, 
-        { 
-          headers: {
-            Authorization: this.session.getToken()
-          } 
+      route,
+      {
+        headers: {
+          Authorization: this.session.getToken()
         }
-      );
+      }
+    );
   }
 
   list(): Observable<TModel[]> {
